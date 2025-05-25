30c874d07d510337620985d428f7079cb7f8ebe4
Lazar Bojanic <bojanic.lazar50@gmail.com>
2025-05-25 16:14:10 +0200

    fixing up react frontend

--- lazar-wordle-clone-frontend-reactjs/src/App.jsx
+++ lazar-wordle-clone-frontend-reactjs/src/App.jsx
@@ -1,25 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import SubmitButton from './SubmitButton';
-import Keyboard from './Keyboard';
-import Board from './Board';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import SubmitButton from "./SubmitButton";
+import Keyboard from "./Keyboard";
+import Board from "./Board";
+
+const ip = import.meta.env.VITE_SERVER_IP;
 
 function App() {
   const [userTries, setUserTries] = useState([]);
-  const [currentWord, setCurrentWord] = useState('');
+  const [currentWord, setCurrentWord] = useState("");
   const [gameOver, setGameOver] = useState(false);
-  const [gameMessage, setGameMessage] = useState('');
+  const [gameMessage, setGameMessage] = useState("");
   const [remainingTries, setRemainingTries] = useState(6);
   const [canProceedToNextTry, setCanProceedToNextTry] = useState(true); // New state
-  const [letterStatuses, setLetterStatuses] = useState(Array(26).fill('WHITE')); // Initialize with default status for all letters
+  const [letterStatuses, setLetterStatuses] = useState(Array(26).fill("WHITE")); // Initialize with default status for all letters
 
   useEffect(() => {
-    console.log(remainingTries)
+    console.log(remainingTries);
     if (remainingTries === 0) {
       setGameOver(true);
-      setCanProceedToNextTry(false)
+      setCanProceedToNextTry(false);
       // Fetch the actual solution word from the API
-      fetch("http://94.189.193.50:5003/api/solutions/getCurrent")
+      fetch(`${ip}/api/game/checkGameStatus?username=dumbe01`)
         .then((response) => response.json())
         .then((data) => {
           setGameMessage(`Game Over! The solution word was: ${data.word}`);
@@ -28,9 +30,9 @@ function App() {
   }, [remainingTries]);
 
   const handleLetterClick = (letter) => {
-    console.log(canProceedToNextTry)
+    console.log(canProceedToNextTry);
     if (!canProceedToNextTry && gameOver) {
-      return
+      return;
     }
     if (currentWord.length < 5) {
       setCurrentWord(currentWord + letter);
@@ -39,58 +41,55 @@ function App() {
 
   const handleBackspaceClick = () => {
     if (!canProceedToNextTry && gameOver) {
-      return
+      return;
     }
     setCurrentWord(currentWord.slice(0, -1));
   };
 
   const handleNewWord = async () => {
-    console.log("1: " + remainingTries)
+    console.log("1: " + remainingTries);
     // Reset the board and clear previous tries
-    setLetterStatuses([])
+    setLetterStatuses([]);
     setUserTries([]);
-    setCurrentWord('');
-    setGameMessage('');
+    setCurrentWord("");
+    setGameMessage("");
     setGameOver(false);
     setRemainingTries(6);
-    console.log("2: " + remainingTries)
+    console.log("2: " + remainingTries);
     setCanProceedToNextTry(true); // Reset canProceedToNextTry
 
     // Fetch a new word from the API
-    const response = await fetch("http://94.189.193.50:5003/api/solutions/new");
+    const response = await fetch(`${ip}/api/solutions/new`);
     const data = await response.json();
     console.log("New Word:", data.word);
   };
 
   const handleSubmit = async () => {
-    
-    setGameMessage("")
+    setGameMessage("");
     if (gameOver) return;
     console.log("Current Word:", currentWord);
     const currentUserTry = {
       word: currentWord,
     };
-    const apiResponse = await fetch("http://94.189.193.50:5003/api/solutions/check", {
-      method: 'POST',
+    const apiResponse = await fetch(`${ip}/api/solutions/check`, {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(currentUserTry)
+      body: JSON.stringify(currentUserTry),
     });
     const userTry = await apiResponse.json();
     console.log("User try: " + JSON.stringify(userTry));
     if (userTry.reason === "solution_not_valid") {
       setCanProceedToNextTry(false);
       setGameMessage("Solution not valid!");
-      console.log(remainingTries)
+      console.log(remainingTries);
       return;
-    }
-    else if (userTry.reason === "solution_correct") {
+    } else if (userTry.reason === "solution_correct") {
       setGameMessage("You Win!");
       setGameOver(true);
       setCanProceedToNextTry(false);
-    } 
-    else if (userTry.reason === "solution_incorrect") {
+    } else if (userTry.reason === "solution_incorrect") {
       setRemainingTries(remainingTries - 1);
       setGameMessage("");
       setCanProceedToNextTry(true);
@@ -98,30 +97,25 @@ function App() {
     setUserTries([...userTries, userTry]);
     const newLetterStatuses = [...letterStatuses];
     userTry.validated_word.letters.forEach((letterObj) => {
-      const letterIndex = letterObj.letter.charCodeAt(0) - 'A'.charCodeAt(0);
+      const letterIndex = letterObj.letter.charCodeAt(0) - "A".charCodeAt(0);
       newLetterStatuses[letterIndex] = letterObj.status;
     });
     setLetterStatuses(newLetterStatuses);
-    setCurrentWord('');
+    setCurrentWord("");
     //console.log(remainingTries)
   };
-
-
 
   return (
     <div className="App">
       <h2>{gameMessage}</h2>
-      <Board
-        userTries={userTries}
-        currentWord={currentWord}
-      />
-      <br/>
+      <Board userTries={userTries} currentWord={currentWord} />
+      <br />
       <Keyboard
         onLetterClick={handleLetterClick}
         onBackspaceClick={handleBackspaceClick}
         letterStatuses={letterStatuses} // Pass the letterStatuses to the Keyboard component
       />
-      <br/>
+      <br />
       <SubmitButton onClick={handleSubmit} />
       <button onClick={handleNewWord}>New Word</button>
     </div>
